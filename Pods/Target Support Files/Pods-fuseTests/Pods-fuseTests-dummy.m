ae1b94a7b2f717e98f85bb3f001b47ab4352ca72
David Rodriguez Luque <dluque.david@gmail.com>
2015-11-16 15:13:27 +0000

    Cocoapods setup

---
+++ Pods/Target Support Files/Pods-fuseTests/Pods-fuseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fuseTests : NSObject
+@end
+@implementation PodsDummy_Pods_fuseTests
+@end
---
+++ Pods/Target Support Files/Pods-fuseUITests/Pods-fuseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fuseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_fuseUITests
+@end
